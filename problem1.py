8e1771168185edf62ef9928109dca0fe9226a065
Kyle Shaw <99025979+shylekaw@users.noreply.github.com>
2022-02-18 13:36:00 +0000

    first send-in

--- problem1.py
+++ problem1.py
@@ -1,6 +1,12 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Kyle Shaw
+# Collaborator(s): None
 #
-
-
+second = input("How many seconds? ")
+day = int(second)//86400
+second = int(second)%86400
+hour = int(second)//3600
+second = int(second)%3600
+minute = int(second)//60
+second = int(second)%60
+print(f"{second} seconds is equivalent to {day} days, {hour} hours, {minute} minutes, and {second} seconds")
--- problem2.py
+++ problem2.py
@@ -1,6 +1,19 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Kyle Shaw
+# Collaborator(s): None
 #
-
-
+startlat = input("Where is your starting latitude? (degrees)")
+startlon = input("Where is your starting longitude? (degrees)")
+endlat = input("Where is your ending latitude? (degrees)")
+endlon = input("Where is your ending longitude? (degrees)")
+import math
+rslat = math.radians(float(startlat))
+rslon = math.radians(float(startlon))
+relat = math.radians(float(endlat))
+relon = math.radians(float(endlon))
+dlon = relon - rslon
+dlat = relat - rslat
+a = (math.sin(dlat/2))**2+math.cos(rslat) * math.cos(relat) * (math.sin(dlon/2))**2
+c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+d = 3956 * c
+print(f"{d} miles away from your starting point")
